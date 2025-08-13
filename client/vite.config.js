89732b444321f61fce38a60fdb93825144519fb7
iBLAK0506 <ezikpejohn098@gmail.com>
2025-08-13 16:53:42 +0100

    nice update

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://real-estate-website-tebr.onrender.com/",
         secure: false,
       },
     },
